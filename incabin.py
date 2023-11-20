f2d13e2fcacb8d4ef3f317f3a0c2e7fc56b0051a
Javier Salado <javier.salado@anyverse.ai>
2023-11-20 17:49:19 +0100

    Fix belts off annotation bug for some brands (5)

--- incabin.py
+++ incabin.py
@@ -2603,7 +2603,7 @@ class InCabinUtils:
             self.setExportAlwaysExcludeOcclusion(beltoff_id)
             belt_name = self._workspace.get_entity_name(beltoff_id)
             seat_info = {}
-            seat_info['number'] = 'seat{}'.format(belt_name.split('_')[2][-2:])
+            seat_info['number'] = 'seat{}'.format(belt_name.split('_')[-2][-2:])
             self.setCustomMetadata(beltoff_id, "Seat", seat_info)
 
     #_______________________________________________________________
